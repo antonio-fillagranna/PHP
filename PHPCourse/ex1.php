76c6235b4e16d3ce07b939fece5f7482de098885
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-05-05 00:03:26 -0300

    Add files via upload

---
+++ PHPCourse/ex1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Minha primeira página com PHP</h1>
+
+<?php
+
+/*
+Um script PHP é executado no servidor, e o resultado em HTML puro é enviado de volta ao navegador.
+Um script PHP pode ser colocado em qualquer lugar do documento.
+Um script PHP começa com <?php e termina com ?>.
+Um script PHP pode conter texto, HTML, CSS, JavaScript, e até mesmo outros scripts PHP dentro dele.
+A extensão de arquivo padrão para arquivos PHP é ".php".  
+Um arquivo PHP normalmente contém tags HTML e algum código de script PHP.
+A função para printar na tela é echo ou print.
+A diferença entre echo e print é que echo pode imprimir múltiplos parâmetros, enquanto print só pode imprimir um.
+As instruções em PHP terminam com um ponto e vírgula (;).
+*/
+
+echo "Hello World!<br>"; //da pra usar \n dependedo de onde está rodando
+echo "Olá mundo!";
+?> 
+<!-- Melhor usar ambos pra evitar a zica! -->
+
+</body>
+</html>
