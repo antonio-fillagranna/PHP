7bef7301fffd8b1183e27888a9ac28d6941c1a95
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-05-06 00:01:40 -0300

    Add files via upload

---
+++ PHPCourse/ex2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<!-- Em PHP, palavras-chave (por exemplo, if, else, while, echo, etc.), classes, funções e funções definidas pelo usuário não são sensíveis a maiúsculas e minúsculas.
+No exemplo abaixo, todas as três declarações echo são iguais e válidas:
+Observação: Como veremos melhor no próximo exemplo todos os nomes de variáveis são sensíveis a maiúsculas e minúsculas!
+-->
+
+<?php
+ECHO "Olá Mundo!<br>";
+echo "Olá Mundo!<br>";
+EcHo "Olá Mundo!<br>";
+?> 
+
+</body>
+</html>
