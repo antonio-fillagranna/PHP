0b81e3c0e3e6451f07a184cde0329dd613095182
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-05-09 00:02:32 -0300

    Add files via upload

---
+++ PHPCourse/ex5.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+/*
+Bloco de comentário de várias linhas
+permite que você escreva comentários
+em várias linhas
+e não precisa usar // ou # em cada linha
+*/
+?>
+ 
+</body>
+</html>
