1f1a01da7a79fd9321d94985441690a9a59bb1c0
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-05-11 23:05:59 -0300

    Add files via upload

---
+++ PHPCourse/ex6.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+// Você pode usar comentários para deixar notas em seu código, ou para desativar partes do código que você não deseja executar.
+$x = 5 /* + 15 */ + 5;
+echo $x;
+?>
+ 
+</body>
+</html>
