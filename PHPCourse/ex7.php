da53cb3af6ee2d7591331604e778b8a8098fe959
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-05-12 22:49:14 -0300

    Add files via upload

---
+++ PHPCourse/ex7.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+/*
+Criando (Declarando) Variáveis em PHP
+No PHP, uma variável começa com o símbolo $, seguido pelo nome da variável.
+-> Observação: Ao atribuir um valor de texto a uma variável, coloque aspas ao redor do valor.
+-> Dica importante: Diferente de outras linguagens de programação, o PHP não possui um comando 
+específico para declarar uma variável. Ela é criada automaticamente no momento em que recebe seu primeiro valor.
+Essa abordagem do PHP torna a manipulação de variáveis mais dinâmica e flexível!
+*/
+$txt = "Olá mundo!";
+$x = 5;
+$y = 10.5;
+
+echo $txt;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+?>
+
+</body>
+</html>
