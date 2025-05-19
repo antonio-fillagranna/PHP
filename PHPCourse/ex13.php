afe7773c4560ded7491bcb2153f661e50c9abe34
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-05-18 22:31:26 -0300

    Add files via upload

---
+++ PHPCourse/ex13.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h2>PHP é Divertido!</h2>";
+echo "Olá mundo!<br>";
+echo "Estou prestes a aprender PHP!<br>";
+echo "Esta ", "string ", "foi ", "feita ", "com múltiplos parâmetros.";
+?>
+
+</body>
+</html>
