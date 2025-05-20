7d5851922f4350c4ad8ab3258b6cb9619ff7183b
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-05-20 19:04:15 -0300

    Add files via upload

---
+++ PHPCourse/ex15.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// PHP é uma linguagem de script do lado do servidor
+// que é usada para desenvolver sites dinâmicos e interativos.
+// exemplos de output usando print
+// print é uma construção de linguagem, não uma função
+print "<h2>PHP é top!</h2>";
+print "Olá mundo!<br>";
+print "Estou estudando PHP!";
+?> 
+
+</body>
+</html>
