98ce9b6bd2d5c11abf455ea0b9ba91d55dc87be8
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-05-25 22:29:04 -0300

    Add files via upload

---
+++ PHPCourse/ex17.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+//Tipos de dados em PHP:
+//String
+$x = "Olá mundo!";
+$y = 'Olá mundo!';
+
+var_dump($x);
+echo "<br>";
+var_dump($y);
+?>
+
+</body>
+</html>
