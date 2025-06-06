8a13955175cfdc98e42f5e2f5dadd0f8baadc84b
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-06-05 21:24:27 -0300

    Add files via upload

---
+++ PHPCourse/ex26.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Exemplo de uso da função strrev() para inverter uma string
+// A função strrev() inverte a ordem dos caracteres em uma string
+$x = "Olá mundo!";
+echo strrev($x);
+?> 
+ 
+</body>
+</html>
