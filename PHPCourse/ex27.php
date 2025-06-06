27fa9c0cf0648b7b89db2c3109a4bc61a9b000fe
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-06-06 00:04:48 -0300

    Add files via upload

---
+++ PHPCourse/ex27.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Exemplo de uso da função strpos() para encontrar a posição de uma substring
+// A função strpos() retorna a posição da primeira ocorrência de uma substring em uma string
+echo strpos("Olá mundo!", "mundo");
+?>
+
+</body>
+</html>
