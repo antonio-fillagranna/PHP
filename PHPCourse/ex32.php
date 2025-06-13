99e94529aa8176cfb7b6f0a32d31c15a515bf55d
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-06-12 23:00:01 -0300

    Add files via upload

---
+++ PHPCourse/ex32.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Cálculo inválido retornará un valor NaN
+// Nota: acos() deve ser usado com valores entre -1 e 1
+
+$x = acos(8);
+var_dump($x);
+?>  
+
+</body>
+</html>
