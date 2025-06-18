05e19d1e8eb226c0c0c43dee3375a028f7821dd2
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-06-17 22:57:17 -0300

    Add files via upload

---
+++ PHPCourse/ex35.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Matemática com PHP
+// PHP possui várias funções matemáticas integradas que permitem realizar operações complexas com facilidade.
+// Aqui estão alguns exemplos de funções matemáticas comuns em PHP:
+// Valor de pi
+echo(pi());
+?>
+
+</body>
+</html>
