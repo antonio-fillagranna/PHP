56815ac310d9862d942226f8aafa624cc136a91d
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-06-19 22:21:22 -0300

    Add files via upload

---
+++ PHPCourse/ex36.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Minimo e Máximo
+// PHP possui funções para encontrar o valor mínimo e máximo em um conjunto de números.
+// Aqui estão alguns exemplos de como usar essas funções:
+echo(min(0, 150, 30, 20, -8, -200) . "<br>");
+echo(max(0, 150, 30, 20, -8, -200));
+?>
+
+</body>
+</html>
