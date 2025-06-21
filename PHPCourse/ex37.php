2492f99aa4d64dc8afcb69dd4c0e99910adfc123
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-06-20 22:54:26 -0300

    Add files via upload

---
+++ PHPCourse/ex37.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Absoluto valor de um número
+// A função abs() retorna o valor absoluto de um número
+// O valor absoluto é o valor sem o sinal negativo
+echo(abs(-6.7));
+?>
+
+</body>
+</html>
