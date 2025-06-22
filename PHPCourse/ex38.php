93c87204a2aef96f04a7f110e919c6c5837c8224
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-06-21 22:02:31 -0300

    Add files via upload

---
+++ PHPCourse/ex38.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Esse código calcula a raiz quadrada de alguns números
+// e exibe o resultado na tela.
+// A função sqrt() é usada para calcular a raiz quadrada.
+// A função retorna um número de ponto flutuante.
+// A função sqrt() aceita apenas números não negativos.
+// Se um número negativo for passado, a função retornará NaN (Not a Number).
+// A função sqrt() é uma função matemática embutida no PHP.
+// Aqui estão alguns exemplos de uso da função sqrt():
+echo(sqrt(64) . "<br>");
+echo(sqrt(0) . "<br>");
+echo(sqrt(1) . "<br>");
+echo(sqrt(9));
+?>
+
+</body>
+</html>
