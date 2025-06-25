f498b8a67852eb5fe7f8f721e4baf0d3765ecf3f
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-06-24 22:47:16 -0300

    Add files via upload

---
+++ PHPCourse/ex40.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Exemplo de código PHP que gera um número aleatório
+// Este código pode ser usado para fins de teste ou demonstração
+// Certifique-se de que o servidor web está configurado para interpretar arquivos PHP
+// e que o arquivo tem a extensão .php
+// Este é um exemplo simples de como gerar um número aleatório
+echo(rand());
+?>
+
+</body>
+</html>
