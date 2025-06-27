45c3d31c8a698510b153bbb31349b46f126c6bed
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-06-27 00:01:08 -0300

    Add files via upload

---
+++ PHPCourse/ex42.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Constantes em PHP são case-sensitive
+// Definindo uma constante com define()
+// A constante SAUDACOES é definida com o valor "Bem vindo ao PHP!"
+// Constantes não podem ser alteradas após serem definidas
+// Constantes são geralmente escritas em letras maiúsculas
+define("SAUDACOES", "Bem vindo ao PHP!");
+echo SAUDACOES;
+?> 
+
+</body>
+</html>
