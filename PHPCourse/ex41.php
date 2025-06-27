1b38bf9ebc7ba5d39b70800730d57e7e5b954dbb
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-06-26 23:25:51 -0300

    Add files via upload

---
+++ PHPCourse/ex41.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Esse código gera um número aleatório entre 10 e 100
+// e o exibe na página.
+// A função rand() é usada para gerar o número aleatório.
+// Já tinha mostrado no exemplo anterior mas agora apresento os parametros
+// que definem o intervalo do número aleatório.
+echo(rand(10, 100));
+?>
+
+</body>
+</html>
