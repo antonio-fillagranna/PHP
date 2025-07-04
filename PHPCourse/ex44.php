8cfa1ffd07343195f730cc21eedbe2bdbf93f640
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-03 23:44:06 -0300

    Add files via upload

---
+++ PHPCourse/ex44.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Aritimética em PHP: soma
+// Este exemplo demonstra a adição de dois números
+// Definindo duas variáveis com valores numéricos
+$x = 10;  
+$y = 6;
+
+echo $x + $y;
+?>  
+
+</body>
+</html>
