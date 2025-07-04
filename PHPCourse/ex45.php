47446ed0da664a05163ab184a169d4b9b3b99b9d
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-04 00:03:57 -0300

    Add files via upload

---
+++ PHPCourse/ex45.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Aritimética em PHP: Subtração
+// Este exemplo demonstra a subtração de dois números
+// Definindo duas variáveis com valores numéricos
+$x = 10;  
+$y = 6;
+
+echo $x - $y;
+?>  
+
+</body>
+</html>
