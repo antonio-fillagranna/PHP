84fec41d5a5ab12375c97a9a9f9188959fdcbf74
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-06 22:38:10 -0300

    Add files via upload

---
+++ PHPCourse/ex46.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Aritimética em PHP: Multiplicação
+// Este exemplo demonstra a multiplicação de dois números
+// e exibe o resultado.
+// A multiplicação é realizada usando o operador *
+$x = 10;  
+$y = 6;
+
+echo $x * $y;
+?>  
+
+</body>
+</html>
