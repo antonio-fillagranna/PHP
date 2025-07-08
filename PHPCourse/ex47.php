e9bb639d30fe0b9fe52e5fbccbf02f07be41e83f
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-07 21:13:13 -0300

    Add files via upload

---
+++ PHPCourse/ex47.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Aritimética em PHP: Divisão
+// Exemplo de divisão de dois números
+// Na maioria das linguagens de programação, temos duas divisões
+// Div e Mod a divisão comum é feita com o operador "/".
+// O operador "%" é usado para o resto da divisão, ou seja, o módulo.
+// Exemplo: 10 / 6 = 1.6666666666667
+// O resto da divisão 10 % 6 = 4
+
+$x = 10;  
+$y = 6;
+
+echo $x / $y;
+?>  
+
+</body>
+</html>
