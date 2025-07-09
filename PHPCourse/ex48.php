dcaa6324945209fc105f97760a32be5054365b56
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-08 23:39:19 -0300

    Add files via upload

---
+++ PHPCourse/ex48.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Esse código demonstra o uso do operador de módulo (%)
+// O operador de módulo retorna o resto da divisão de dois números
+// Neste exemplo, 15 dividido por 4 tem um resto de 3
+$x = 15;
+$x %= 4;
+
+echo $x;
+?>  
+
+</body>
+</html>
