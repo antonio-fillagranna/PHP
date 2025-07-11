2f94b8b22ccb92dbda04d283ffbb30aef10eb392
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-10 22:47:20 -0300

    Add files via upload

---
+++ PHPCourse/ex50.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Atribuição de valor em PHP
+// O operador '=' é usado para atribuição de valor a uma variável
+// Neste exemplo, estamos atribuindo o valor 10 à variável $x
+// Depois, estamos exibindo o valor de $x usando echo
+$x = 10;  
+echo $x;
+?>  
+
+</body>
+</html>
