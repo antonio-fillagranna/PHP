b05fd8b9365b4ccf922f48e9be36e9afee7e1039
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-12 11:53:07 -0300

    Add files via upload

---
+++ PHPCourse/ex52.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Este código demonstra o uso do operador de atribuição composto -= em PHP.
+// O operador -= subtrai o valor da variável existente.
+// Neste exemplo, começamos com $x igual a 50 e depois subtraímos 30 dele.
+$x = 50;
+$x -= 30;
+
+echo $x;
+?>  
+
+</body>
+</html>
