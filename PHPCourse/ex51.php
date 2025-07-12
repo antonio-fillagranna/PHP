0003afabdde36e1e00d17dde38a238eddeb93557
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-11 21:43:45 -0300

    Add files via upload

---
+++ PHPCourse/ex51.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Esse código demonstra o uso do operador de atribuição composto += em PHP.
+// O operador += adiciona o valor à variável existente.
+// Neste exemplo, começamos com $x igual a 20 e depois adicionamos 100 a ele.
+$x = 20;  
+$x += 100;
+
+echo $x;
+?>  
+
+</body>
+</html>
