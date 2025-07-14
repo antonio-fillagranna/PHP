b6b416150392fbdf6170daff78a5ca88a3ba282e
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-13 22:49:24 -0300

    Add files via upload

---
+++ PHPCourse/ex53.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Este código demonstra o uso do operador de atribuição composto *= em PHP.
+// O operador *= multiplica o valor da variável existente.
+// Neste exemplo, começamos com $x igual a 5 e depois multiplicamos por 6.
+$x = 5;
+$x *= 6;
+
+echo $x;
+?>  
+
+</body>
+</html>
