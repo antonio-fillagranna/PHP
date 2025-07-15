5785f17140292ed902c3fe4afb97460af9bd2d06
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-14 23:39:41 -0300

    Add files via upload

---
+++ PHPCourse/ex54.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Esse código PHP demonstra o uso do operador de atribuição de divisão
+// O operador /= divide o valor da variável pela direita pelo valor da variável pela esquerda
+// e atribui o resultado à variável da esquerda.
+$x = 10;
+$x /= 5;
+
+echo $x;
+?>  
+
+</body>
+</html>
