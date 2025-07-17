ba4855323fd148347299a072997b6de2e824ce5b
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-16 22:04:21 -0300

    Add files via upload

---
+++ PHPCourse/ex56.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Esse código verifica se dois valores são diferentes usando o operador !=
+// O operador != compara os valores sem considerar o tipo
+$x = 100;  
+$y = "100";
+
+var_dump($x != $y);
+?>  
+
+</body>
+</html>
