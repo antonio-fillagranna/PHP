f9dd34660847e660bfc683cb9134fb1e86f86f76
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-21 23:36:45 -0300

    Add files via upload

---
+++ PHPCourse/ex59.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Esse código verifica se $x é maior que $y e exibe o resultado
+// Se $x for maior que $y, o resultado será true, caso contrário, será
+// false.
+// O operador utilizado é o "maior que" (>).
+$x = 100;
+$y = 50;
+
+var_dump($x > $y);
+?>  
+
+</body>
+</html>
