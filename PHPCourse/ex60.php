b9a56a170b8dda9687d20084343ef38582cd2c1d
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-22 22:17:17 -0300

    Add files via upload

---
+++ PHPCourse/ex60.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Esse é um exemplo de comparação de valores em PHP
+// Aqui, estamos comparando dois números inteiros
+// O resultado será um booleano (true ou false)
+// Neste caso, estamos verificando se $x é menor que $y
+// Se $x for menor que $y, o resultado será true, caso contrário, será false
+// O resultado será exibido usando var_dump
+$x = 10;
+$y = 50;
+
+var_dump($x < $y);
+?>  
+
+</body>
+</html>
