e6bfc3dc42f3c1688f85d736204bc191d01f260e
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-24 19:24:38 -0300

    Add files via upload

---
+++ PHPCourse/ex62.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Esse código verifica se $x é menor ou igual a $y
+// e exibe o resultado como um booleano.
+// Se $x for menor ou igual a $y, o resultado será true; caso contrário, será false.
+// Neste exemplo, $x é 50 e $y é 50, então o resultado será true.
+// O operador <= é usado para comparar os valores.
+$x = 50;
+$y = 50;
+
+var_dump($x <= $y);
+?>  
+
+</body>
+</html>
