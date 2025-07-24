d68ab84296e36c5e0b51e2a96c87389ea6228705
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-23 23:14:53 -0300

    Add files via upload

---
+++ PHPCourse/ex61.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Esse código verifica se $x é maior ou igual a $y
+// e exibe o resultado como um booleano.
+// Se $x for maior ou igual a $y, o resultado será true; caso contrário, será false.
+$x = 50;
+$y = 50;
+
+var_dump($x >= $y);
+?>  
+
+</body>
+</html>
