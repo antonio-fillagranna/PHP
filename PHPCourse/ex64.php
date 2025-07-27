dc514f140d484554feb8dadd3469e3a64fdda1ce
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-26 21:15:34 -0300

    Add files via upload

---
+++ PHPCourse/ex64.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Esse código demonstra o uso do operador de incremento pré-fixo em PHP.
+// O operador ++ antes da variável incrementa o valor da variável antes de usá-la.
+// Neste exemplo, a variável $x é incrementada em 1 antes de ser exibida.
+// O resultado será 11, pois $x é incrementado de 10 para 11
+$x = 10;  
+echo ++$x;
+?>  
+
+</body>
+</html>
