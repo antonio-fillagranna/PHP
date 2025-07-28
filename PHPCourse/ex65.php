31982dff3bfc22da974f53bdc36cf16efc55893c
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-27 22:02:52 -0300

    Add files via upload

---
+++ PHPCourse/ex65.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Esse código demonstra o uso do operador de incremento pós-fixado em PHP.
+// O operador $x++ incrementa o valor de $x, mas retorna o valor antes
+// do incremento. Portanto, o valor exibido será o valor original de $x antes do incremento.
+// Após a execução, $x terá o valor incrementado em 1.
+$x = 10;  
+echo $x++;
+?>  
+
+</body>
+</html>
