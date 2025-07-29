7e1ccd4a81830ef77d424ce692d15d841372ba7b
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-28 22:14:22 -0300

    Add files via upload

---
+++ PHPCourse/ex66.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Esse código demonstra o uso do operador de decremento pré-fixo em PHP.
+// O operador --$x decrementa o valor de $x em 1 antes de usá-lo na expressão.
+// Neste exemplo, $x é inicializado com o valor 10, e então é decrementado para 9 antes de ser exibido.
+// O resultado será 9.
+$x = 10;  
+echo --$x;
+?>  
+
+</body>
+</html>
