593bd0b1921024ba39263ff1f1ff224420ce258d
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-30 00:11:53 -0300

    Add files via upload

---
+++ PHPCourse/ex68.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Operadores lógicos: E </h1>
+
+<p>Vai escrever a mensagem se as duas condições forem verdadeiras.</p>
+
+<?php
+$x = 100;  
+$y = 50;
+
+if ($x == 100 and $y == 50) {
+    echo "Olá mundo!";
+}
+?>  
+
+</body>
+</html>
