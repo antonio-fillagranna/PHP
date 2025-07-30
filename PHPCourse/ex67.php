6f7e4ae876b4629e7fa57f332f9cd635266dc222
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-29 22:50:22 -0300

    Add files via upload

---
+++ PHPCourse/ex67.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Esse código demonstra o uso do operador de decremento pós-fixado em PHP.
+// O operador de decremento pós-fixado (--) diminui o valor da variável após 
+// a expressão ser avaliada. Neste exemplo, o valor de $x é 10, e ao usar
+// o operador $x--, o valor 10 é retornado antes de $x ser decrementado para 9.
+// Portanto, a saída será 10.
+$x = 10;  
+echo $x--;
+?>  
+
+</body>
+</html>
