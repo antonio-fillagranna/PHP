7cc7b7dbd3bba6dea8655185803d5e7d0cc5273d
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-07-31 23:03:28 -0300

    Add files via upload

---
+++ PHPCourse/ex69.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Operadores lógicos: OU </h1>
+
+<p>Vai escrever a mensagem se pelo menos uma das condições forem verdadeiras.</p>
+
+<?php
+$x = 100;  
+$y = 50;
+
+if ($x == 100 or $y == 80) {
+    echo "Olá mundo!";
+}
+?>  
+
+</body>
+</html>
