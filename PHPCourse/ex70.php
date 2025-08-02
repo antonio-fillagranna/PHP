da5d77039073000b4110f9bd679ac2bae2de3c97
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-08-01 21:32:13 -0300

    Add files via upload

---
+++ PHPCourse/ex70.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Operadores lógicos: XOR </h1>
+
+<p>Vai escrever a mensagem se APENAS UMA das condições for verdadeira, se ambas forem não imprime.</p>
+
+<?php
+$x = 100;  
+$y = 50;
+
+if ($x == 100 xor $y == 80) {
+    echo "Olá mundo!";
+}
+?>  
+
+</body>
+</html>
