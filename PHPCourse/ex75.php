e1fff2af37f9bb06e593076fd3e9b4333dba9c0c
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-08-07 20:16:53 -0300

    Add files via upload

---
+++ PHPCourse/ex75.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Esse exemplo demonstra a concatenação de strings em PHP
+// Usando o operador de concatenação (.) para unir duas strings
+// Aqui mostra como usar o operador de atribuição de concatenação (.=)
+// Pareceido com os operadores += e -= mas para strings
+// O resultado será "Olá mundo!"
+// A variável $txt1 contém "Olá" e $txt2 contém " mundo!"
+$txt1 = "Olá";
+$txt2 = " mundo!";
+$txt1 .= $txt2;
+echo $txt1;
+?>  
+
+</body>
+</html>
