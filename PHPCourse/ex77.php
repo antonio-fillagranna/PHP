ac6f5ec7527923913678b0668866077f4e6614f1
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-08-09 11:34:14 -0300

    Add files via upload

---
+++ PHPCourse/ex77.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Operador igual (==)
+// Compara os valores de dois arrays (sem considerar as chaves) e retorna true se forem iguais.
+// Compara os valores e as chaves de dois arrays e retorna true se forem idênticos.
+$x = array("a" => "vermelho", "b" => "verde");  
+$y = array("c" => "azul", "d" => "amarelo");  
+
+var_dump($x == $y);
+?>  
+
+</body>
+</html>
