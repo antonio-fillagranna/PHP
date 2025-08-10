6e8c67c9370ecc4e34acd7a5fa824380c376d459
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-08-10 20:58:39 -0300

    Add files via upload

---
+++ PHPCourse/ex78.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Operador identico (===)
+// Compara os valores e os tipos dos arrays 
+// Retorna true se ambos os arrays forem iguais e do mesmo tipo
+// Retorna false caso contrário
+$x = array("a" => "vermelho", "b" => "verde");  
+$y = array("c" => "azul", "d" => "amarelo");  
+
+var_dump($x === $y);
+?>  
+
+</body>
+</html>
