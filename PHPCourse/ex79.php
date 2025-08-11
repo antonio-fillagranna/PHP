26362d1f6fb0bfa57db117b41dbd5bc01da653b1
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-08-11 00:04:29 -0300

    Add files via upload

---
+++ PHPCourse/ex79.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Operador diferente (!=)
+// Compara dois valores e retorna true se eles não forem iguais.
+// Se forem iguais, retorna false.
+// Valores diferentes, mas do mesmo tipo, são considerados diferentes.
+$x = array("a" => "vermelho", "b" => "verde");  
+$y = array("c" => "azul", "d" => "amarelo");  
+
+var_dump($x != $y);
+?>  
+
+</body>
+</html>
