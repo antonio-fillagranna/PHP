4b936958ce7b27adb33ba1bd6bbfba3e1ef9b466
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-08-12 00:37:07 -0300

    Add files via upload

---
+++ PHPCourse/ex80.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Operador diferente (<>)
+// Retorna true se os operandos não são iguais
+// e false se eles são iguais
+// Assim como já foi mostrado ele é equivalente ao operador != em vetores (arrays) também
+$x = array("a" => "vermelho", "b" => "verde");  
+$y = array("c" => "azul", "d" => "amarelo");  
+
+var_dump($x <> $y);
+?>  
+
+</body>
+</html>
