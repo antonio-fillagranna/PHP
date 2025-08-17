45ed071f9eaf799e3bf64c781f3107d9c037c8e3
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-08-17 10:58:57 -0300

    Add files via upload

---
+++ PHPCourse/ex85.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$t = date("H");
+// Condicional ELSE (SE NÃO)
+// Usada sempre com o IF (SE)
+// o if geralmente é usado sem o else, mas o else nunca é usado sem o if    
+
+if ($t < "20") {
+  echo "Tenha um bom dia!";
+} else {
+  echo "Tenha uma boa noite!";
+}
+?>
+
+</body>
+</html>
