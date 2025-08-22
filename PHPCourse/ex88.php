0d29c45276d6b47ec3eae9cd8990ecbff85e0682
Antônio Fillagranna <131285979+antonio-fillagranna@users.noreply.github.com>
2025-08-21 23:32:44 -0300

    Add files via upload

---
+++ PHPCourse/ex88.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Loop while
+// Seta a variável $x com o valor 1
+// Enquanto $x for menor ou igual a 5, o bloco de código dentro do while será executado
+// A cada iteração, o valor de $x é incrementado em 1
+$x = 1;
+
+while($x <= 5) {
+  echo "O número é: $x <br>";
+  $x++;
+}
+?>
+
+</body>
+</html>
